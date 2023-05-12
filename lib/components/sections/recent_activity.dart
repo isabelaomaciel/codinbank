150ec08254a45c185d684c587c95de0d23a0e184
Isabela Oliveira Maciel <isabelasouxx@gmail.com>
2023-05-11 21:37:39 -0300

    fix: text local

--- lib/components/sections/recent_activity.dart
+++ lib/components/sections/recent_activity.dart
@@ -21,11 +21,12 @@ class _RecentActivityContent extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        const Text('Total Gasto'),
+
         const Text(
           '\$ 9900,97',
           style: TextStyle(fontSize: 28),
         ),
-        const Text('Total Gasto'),
 
         const Padding(
           padding: EdgeInsets.only(top: 8.0, bottom: 8.0),
